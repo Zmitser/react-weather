a43f99d6e50826f6d7dd05ded1b8a7d1093ff095
zmitserkoskinen <dzmitry.borysovets@gmail.com>
2016-08-03 15:52:37 +0300

    deploy in heroku

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+app.use(function (req, res, next) {
+    if (req.headers['x-forward-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.URL)
+    }
+});
 app.use(express.static('public'));
-app.listen(3000, function () {
-    console.log("Express server is up on port 3000")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log("Express server is up on port " + PORT)
 });
